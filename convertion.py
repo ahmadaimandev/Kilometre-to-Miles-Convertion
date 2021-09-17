25df2c383babe93cd8dd16f4ad0a5cfbd441346e
Ahmad-Aiman <66258284+Ahmad-Aiman@users.noreply.github.com>
2021-09-17 20:46:07 +0800

    Add files via upload

---
+++ convertion.py
@@ -0,0 +1,15 @@
+#1 km = 1000 m
+#1 km = 0.621 miles
+
+Kilo_KM = float(input("Enter in Kilometre (KM): "))
+
+Kilo_KM = float (Kilo_KM)
+
+KilometretoMeter = Kilo_KM * 1000
+KilometretoMiles = Kilo_KM * 0.621
+
+print("Value in Metre: ", KilometretoMeter , "Metre")
+print("Value in Miles: ", KilometretoMiles, "Miles")
+
+
+
